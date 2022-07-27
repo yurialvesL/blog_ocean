f51322d3b51aefdb49a25aa5406f0b770c411601
Yuri Alves <yurisoad2015@gmail.com>
2022-07-27 00:33:24 +0000

    adição de templates

--- app.py
+++ app.py
@@ -1,14 +1,36 @@
 from flask import Flask,render_template
+from datetime import datetime
 
 app = Flask("hello")
 
+posts = [
+    {
+        'title': 'O meu primeiro Post',
+        'body': 'Aqui é o texto do post',
+        'author': 'Yuri',
+        'created' : datetime(2022,7,25)
+
+    },
+    {
+        'title': 'Segundo Post',
+        'body': 'Aqui é o texto do post',
+        'author': 'Camilla',
+        'created' : datetime(2022,7,26)
+
+    }
+]
+
 @app.route("/")
-@app.route("/hello")
-def hello():
-    return "Hello Word"
+def index():
+    return render_template('index.html', posts=posts)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
-@app.route("/meucontato")
-def meuContato():
-    return render_template('index.html')
+
+
+
+
 
