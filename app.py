6c53a37932567cd3197027e183f1f4c4e2222195
Yuri Alves <yurisoad2015@gmail.com>
2022-07-30 00:00:39 +0000

    primeiro deploy

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask,render_template, redirect, url_for, request, flash
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user
+from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user, login_required
 from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy.exc import IntegrityError
+import os
 
 app = Flask("hello")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+db_url = os.environ.get("DATABSE_URL") or "sqlite:///app.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url.replace('postgres','postgresql')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "sugoi"
 
@@ -70,7 +72,7 @@ def register():
      return render_template('register.html')
 
 
-@app.route('/login',  methods=["POST"])
+@app.route('/login', methods=["GET","POST"])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -92,3 +94,22 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/create', methods=["GET","POST"])
+@login_required
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        body = request.form['body']
+
+        try:
+            post = Post(title=title, body=body, author=current_user)
+            db.session.add(post)
+            db.session.commit()
+            return redirect(url_for('index'))
+        except IntegrityError:
+            flash("Error on create Post, try again later")
+
+    return render_template('create.html')
+
